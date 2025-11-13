601e3f6398fa5b2cde0cde9182db063aa04e6f59
Dieguin_01 <frostleanminecraft@gmail.com>
2025-11-13 18:38:22 +0000

    TOMI 🥶

--- app_flask.py
+++ app_flask.py
@@ -1,13 +1,41 @@
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
-
+app=Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-## Continue o código aqui.
+@app.route("/soma")
+def smr():
+    vlr_a=request.args.get("valor1", type=int)
+    vlr_b=request.args.get("valor2", type=int)
+    total=vlr_a+vlr_b
+    return {"redultado" : total}
 
-if __name__ == "__main__":
+@app.route("/subtrair")
+def sub():
+    vlr_a=request.args.get("valor1", type=int)
+    vlr_b=request.args.get("valor2", type=int)
+    total=vlr_a-vlr_b
+    return {"resultado":total}
+
+@app.route("/multi")
+def multi():
+    vlr_a=request.args.get("valor1", type=int)
+    vlr_b=request.args.get("valor2", type=int)
+    total=vlr_a*vlr_b
+    return {"resultado":total}
+
+@app.route("/divisao")
+def div():
+    vlr_a=request.args.get("valor1", type=int)
+    vlr_b=request.args.get("valor2", type=int)
+    if vlr_b==0:
+        return "Erro de divisão com zero"
+    total=vlr_a/vlr_b
+    return {"resultado":total}
+
+
+if __name__=="__main__":
     app.run(debug=True)
